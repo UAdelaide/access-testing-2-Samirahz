ae18144975e222beae068cc401be39ae2f5b7014
Samira <a1852922@adelaide.edu.au>
2025-06-16 06:11:57 +0000

    [automatic commit] 2025-06-16 06:11:56 UTC 121.44.208.60

--- part1/db.js
+++ part1/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-    host: 'localhost'
-    user: 'root'
+    host: 'localhost',
+    user: 'root',
     password: 'yourpassword'
     database: 'textbook_marketplace'
 });
 
-module
+module.exports - db;
