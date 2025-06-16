3273117915474ff021730b64137759a58940dda8
Samira <a1852922@adelaide.edu.au>
2025-06-16 06:18:45 +0000

    [automatic commit] 2025-06-16 06:18:45 UTC 121.44.208.60

--- part1/routes/messages.js
+++ part1/routes/messages.js
@@ -7,11 +7,11 @@ var CURRENT_BUYER_ID = 1;
 var CURRENT_SELLER_ID = 2;
 
 router.get('/items', async (req, res) => {
-    const [rows] = await db.query('
+    const [rows] = await db.query(`
         SELECT bl.BookID, bi.Title, u.Name AS SellerName
         FROM BookListings bl
         JOIN BookInfo bi ON bl.BookInfoID = bi.BookInfoID
         JOIN Users u ON bl.SellerID = u.CURRENT_SELLER_ID
-        ');
+        `);
         res.json(rows);
 });
