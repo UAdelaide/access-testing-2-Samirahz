07b25acb71714483129f937ab3e41084709f026c
Samira <a1852922@adelaide.edu.au>
2025-06-16 06:14:48 +0000

    [automatic commit] 2025-06-16 06:14:48 UTC 121.44.208.60

--- part1/routes/messages.js
+++ part1/routes/messages.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var route = express.Router();
 var db = require('../db');
+const { router } = require('../app');
 
 var CURRENT_BUYER_ID = 1;
 var CURRENT_SELLER_ID = 2;
 
+router.get('/items')
