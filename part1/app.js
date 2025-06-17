cc737dc0a53e7bfcb32e470bbc05bfb474fb9858
Samira <a1852922@adelaide.edu.au>
2025-06-17 01:04:38 +0000

    [automatic commit] 2025-06-17 01:04:37 UTC 121.44.208.60

--- part1/app.js
+++ part1/app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var messageRouter = require('/.routes/message');
+var messageRouter = require('./routes/message');
 
 var app = express();
 
