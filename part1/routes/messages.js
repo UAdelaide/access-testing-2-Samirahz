377753fb5910927923677a0ffc65c8ce061ce919
Samira <a1852922@adelaide.edu.au>
2025-06-17 01:47:06 +0000

    [automatic commit] 2025-06-17 01:47:06 UTC 121.44.208.60

--- part1/routes/messages.js
+++ part1/routes/messages.js
@@ -16,7 +16,7 @@ router.get('/items', async (req, res) => {
 });
 
 router.post('/messages', async (req, res) => {
-const { bookID, message } = req.body;
+const { bookID, SellerID, message } = req.body;
 await db.query(`
     INSERT INTO Messages (BuyerID< SellerID< BookID< MessageText, SentAt)
     VALUES (?, ?, ?, ?, NOW())
