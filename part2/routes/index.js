66dc23c58bb1dfc0ef8280482f72b013cdf36f9a
Samira <a1852922@adelaide.edu.au>
2025-06-19 05:22:32 +0000

    [automatic commit] 2025-06-19 05:22:31 UTC 121.44.208.60

--- part2/routes/index.js
+++ part2/routes/index.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
 router.get('/posts', function (req, res, next) {
-
   if ('user' in req.session) {
     console.log(req.session.user);
   }
