0f953148a2ae66d33219a399ebfdb1e5f25e7a63
Samira <a1852922@adelaide.edu.au>
2025-06-19 05:28:43 +0000

    [automatic commit] 2025-06-19 05:28:43 UTC 121.44.208.60

--- part2/routes/index.js
+++ part2/routes/index.js
@@ -31,42 +31,34 @@ router.get('/posts', function (req, res) {
       LEFT JOIN q_tags ON q_tags.question = questions.q_id
       LEFT JOIN q_up ON q_up.question = questions.q_id;
     `;
-
     connection.query(query, function (err, rows) {
       connection.release();
       if (err) {
         res.sendStatus(500);
         return;
       }
-
       for (const row of rows) {
         row.tags = row.tags ? row.tags.split(',') : [];
       }
-
       res.json(rows);
     });
   });
 });
-
 /* POST add post */
 router.post('/addpost', function (req, res) {
   const { title, content, tags } = req.body;
-
   if (title && content && Array.isArray(tags) && req.session && req.session.user) {
     const author = req.session.user;
-
     req.pool.getConnection(function (err, connection) {
       if (err) {
         console.error(err);
         res.sendStatus(500);
         return;
       }
-
       const insertQuestionQuery = `
         INSERT INTO questions (author, title, content, timestamp)
         VALUES (?, ?, ?, NOW());
       `;
-
       connection.query(insertQuestionQuery, [author.u_id, title, content], function (err) {
         if (err) {
           console.error(err);
@@ -74,13 +66,11 @@ router.post('/addpost', function (req, res) {
           res.sendStatus(500);
           return;
         }
-
         if (tags.length === 0) {
           connection.release();
           res.end();
           return;
         }
-
         const tagValues = tags.map(tag => `('${tag}', LAST_INSERT_ID())`).join(',');
         const insertTagsQuery = `INSERT INTO question_tags (tagname, question) VALUES ${tagValues};`;
         connection.query(insertTagsQuery, function (err) {
